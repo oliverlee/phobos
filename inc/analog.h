371692482d87e8d076bf20e80564ba82b4e20672
Oliver Lee <oliverzlee@gmail.com>
2016-05-18 17:49:09 +0200

    Add Analog sensor class
    Add Analog sensor class that samples channels ADC10, ADC11, ADC12 at
    1 kHz.

---
+++ inc/analog.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "hal.h"
+#include <array>
+
+class Analog {
+    public:
+        Analog();
+        void start();
+        adcsample_t get_adc10() const;
+        adcsample_t get_adc11() const;
+        adcsample_t get_adc12() const;
+        static adc_channels_num_t buffer_size();
+
+    private:
+        enum sensor_t {ADC10=0, ADC11, ADC12};
+        static constexpr adc_channels_num_t m_adc_buffer_size = 3;
+        std::array<adcsample_t, m_adc_buffer_size> m_adc_buffer;
+};
