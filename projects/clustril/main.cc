d027cf4fdc979c0fed6ea211486426a17756d99e
Oliver Lee <oliverzlee@gmail.com>
2016-10-10 14:49:18 +0200

    Wrap angles to range [-pi, pi) radians

--- projects/clustril/main.cc
+++ projects/clustril/main.cc
@@ -72,17 +72,14 @@ namespace {
     };
 
     model::real_t wrap_angle(model::real_t angle) {
-        /*
-         * Angle magnitude is assumed to small enough to NOT require multiple
-         * additions/subtractions of 2pi.
-         */
-        if (angle >= boost::math::constants::pi<float>()) {
-            angle -= boost::math::constants::two_pi<float>();
+        model::real_t a = std::fmod(angle, boost::math::constants::two_pi<model::real_t>());
+        if (a >= boost::math::constants::pi<model::real_t>()) {
+            a -= boost::math::constants::two_pi<model::real_t>();
         }
-        if (angle < -boost::math::constants::pi<float>()) {
-            angle += boost::math::constants::two_pi<float>();
+        if (a < -boost::math::constants::pi<model::real_t>()) {
+            a += boost::math::constants::two_pi<model::real_t>();
         }
-        return angle;
+        return a;
     }
 
      float rad_to_deg(float angle) {
@@ -178,8 +175,8 @@ int main(void) {
      */
     rtcnt_t kalman_update_time = 0;
     bool print_version_string;
+    u.setZero(); /* set both roll and steer torques to zero */
     while (true) {
-        u.setZero(); /* set both roll and steer torques to zero */
         u[1] = static_cast<float>(analog.get_adc12()*2.0f*max_kistler_torque/4096 -
                 max_kistler_torque);
         float motor_torque = static_cast<float>(
@@ -187,7 +184,7 @@ int main(void) {
                 max_kollmorgen_torque);
 
         /* set measurement vector */
-        z[0] = x[0]; /* yaw angle, just use previous state value */
+        z[0] = wrap_angle(x[0]); /* yaw angle, just use previous state value */
 
         /*
          * Steer angle, read from encoder (enccnt_t is uint32_t)
@@ -231,7 +228,7 @@ int main(void) {
             printf("sensors:\t%0.3f\t%0.3f\t%0.3f\t%0.3f\r\n",
                     u[1], motor_torque, rad_to_deg(z[0]), rad_to_deg(z[1]));
             printf("state:\t%0.2f\t%0.2f\t%0.2f\t%0.2f\t%0.2f\r\n",
-                    x[0], x[1], x[2], x[3], x[4]);
+                    rad_to_deg(x[0]), rad_to_deg(x[1]), rad_to_deg(x[2]), rad_to_deg(x[3]), rad_to_deg(x[4]));
             printf("kalman update time: %U us\r\n",
                     RTC2US(STM32_SYSCLK, kalman_update_time));
         } else if (s == printst_t::NONE) {
