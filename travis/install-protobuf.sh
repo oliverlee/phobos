b76a920747866802b4ab9ff2800da7f62b57e8d0
Oliver Lee <oliverzlee@gmail.com>
2016-10-13 11:28:07 +0200

    Build Python protobuf implementation on Travis

--- travis/install-protobuf.sh
+++ travis/install-protobuf.sh
@@ -6,6 +6,7 @@ if [ ! -d "$HOME/protobuf/lib" ]; then
     wget https://github.com/google/protobuf/releases/download/v2.6.1/protobuf-2.6.1.tar.gz;
     tar -xf protobuf-2.6.1.tar.gz;
     cd protobuf-2.6.1 && ./configure --prefix=$HOME/protobuf && make && make install
+    cd python && python setup.py build --cpp_implementation && python setup.py install --cpp_implementation --user
     cd $HOME;
 else
     echo 'Using cached protobuf directory.';
