74599c2b0f233f84bda5ef74842af103e1512a62
Oliver Lee <oliverzlee@gmail.com>
2016-11-22 16:26:13 +0100

    Revert "Store state when position is adjusted"
    This reverts commit 5244f8b309cb470d9cf50189052198062d1f2b38.

--- inc/tsencoder.h
+++ inc/tsencoder.h
@@ -45,6 +45,7 @@ class TSEncoder {
         enum class state_t {
             STOP, READY
         };
+
         enum class index_t {
             NONE, NOTFOUND, FOUND
         };
@@ -62,10 +63,6 @@ class TSEncoder {
 
     private:
         using event_t = std::pair<rtcnt_t, tsenccnt_t>;
-        enum class position_result_t {
-            NONE, ESTIMATED, ADJUSTED
-        };
-
         mutable std::array<event_t, N> m_events; /* circular buffer for encoder events */
         mutable size_t m_event_index; /* index of oldest entry */
         mutable size_t m_skip_order_counter; /* skip order counter */
