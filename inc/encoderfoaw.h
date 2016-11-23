d87c55ea8ebc5ff586e78f2021dfb24d0654b301
Oliver Lee <oliverzlee@gmail.com>
2016-11-23 14:47:32 +0100

    Add encoder sample input queue
    Add an input queue to prevent encoder sample buffer from changing during
    a velocity calculation.

--- inc/encoderfoaw.h
+++ inc/encoderfoaw.h
@@ -14,11 +14,20 @@ class EncoderFoaw: public Encoder {
         T velocity() const;
 
     private:
+        static constexpr size_t m_input_buffers_number = 4;
+        static constexpr size_t m_input_buffers_size = sizeof(T);
+
+        uint8_t m_ib[BQ_BUFFER_SIZE(m_input_buffers_number, m_input_buffers_size)];
+        THD_WORKING_AREA(m_wa_sample_handler_thread, 128);
         Foaw<T, N> m_estimator;
+        thread_t* m_sample_thread;
+        input_buffers_queue_t m_ibqueue;
+        mutable binary_semaphore_t m_buffer_semaphore;
         virtual_timer_t m_sample_timer;
         const systime_t m_sample_period;
 
         static void sample_callback(void* p);
+        static void sample_ibqueue_handler(void* p);
 };
 
 #include "encoderfoaw.hh"
