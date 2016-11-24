2a5ad0d96cdfed2632945fdffdd6d2653d66929e
Oliver Lee <oliverzlee@gmail.com>
2016-11-24 10:27:42 +0100

    Define function foaw::estimate_velocity
    Define function estimate_velocity() in foaw namespace that uses the
    First-Order Adaptive Windowing algorithm to estimate velocity. This
    allows use of the algorithm with a circular buffer of samples that is
    not managed by the Foaw class.

--- inc/foaw.h
+++ inc/foaw.h
@@ -16,20 +16,28 @@
  *  N is the maximum window size (size of buffer of sampled positions)
  */
 
+namespace foaw {
+
+template <typename T, size_t N>
+T estimate_velocity(const std::array<T, N>& circular_buffer,
+        size_t oldest_index, T sample_period, T allowed_error);
+
+} // namespace foaw
+
 template <typename T, size_t N>
 class Foaw {
     public:
         Foaw(T sample_period, T allowed_error);
         void reset();
         void add_position(T x);
-        T estimate_velocity() const;
+        T estimate_velocity();
         T sample_period() const;
         T allowed_error() const;
 
     private:
         std::array<T, N> m_positions; /* circular buffer for sampled positions */
         size_t m_position_index; /* index of oldest entry */
-        mutable T m_velocity; /* last calculated velocity */
+        T m_velocity; /* last calculated velocity */
         T m_T; /* sample period, s */
         T m_d; /* allowed error between sample and best fit line, counts*/
 };
--- src/foaw.hh
+++ src/foaw.hh
@@ -1,4 +1,47 @@
+#include <cassert>
 #include <cmath>
+
+namespace foaw {
+
+template <typename T, size_t N>
+T estimate_velocity(const std::array<T, N>& circular_buffer, size_t oldest_index, T sample_period, T allowed_error) {
+    assert(sample_period > static_cast<T>(0));
+    assert(allowed_error > static_cast<T>(0));
+    assert(oldest_index < N);
+
+    size_t newest_index = oldest_index - 1;
+    if (oldest_index == 0) {
+        newest_index = N - 1;
+    }
+
+    T velocity = 0.0;
+    for (int n = 1; n < static_cast<int>(N); ++n) {
+        T bn = 0.0;
+        for (int i = 0; i <= n; ++i) {
+            bn += (n - 2*i)*circular_buffer[(newest_index - i + N) % N];
+        }
+        bn /= sample_period*n*(n + 1)*(n + 2)/6;
+
+        /*
+         * The definition of 'an' in the paper doesn't make much sense so assume
+         * line passes through last sampled position (which may not be the case
+         * with a best fit line).
+         */
+        T an = circular_buffer[newest_index];
+        for (int j = 1; j < n; ++j) {
+            T ykj = an - bn * j * sample_period;
+            T error = circular_buffer[(newest_index - j + N) % N] - ykj;
+            if (std::abs(error) > allowed_error) {
+                return velocity;
+            }
+
+        }
+        velocity = bn;
+    }
+    return velocity;
+}
+
+} // namespace foaw
 
 /*
  * Member function definitions of Foaw template class.
@@ -26,36 +69,8 @@ void Foaw<T, N>::add_position(T x) {
 }
 
 template <typename T, size_t N>
-T Foaw<T, N>::estimate_velocity() const {
-    size_t last_index = m_position_index - 1;
-    if (m_position_index == 0) {
-        last_index = N - 1;
-    }
-
-    m_velocity = 0.0;
-    for (int n = 1; n < static_cast<int>(N); ++n) {
-        T bn = 0.0;
-        for (int i = 0; i <= n; ++i) {
-            bn += (n - 2*i)*m_positions[(last_index - i + N) % N];
-        }
-        bn /= m_T*n*(n + 1)*(n + 2)/6;
-
-        /*
-         * The definition of 'an' in the paper doesn't make much sense so assume
-         * line passes through last sampled position (which may not be the case
-         * with a best fit line).
-         */
-        T an = m_positions[last_index];
-        for (int j = 1; j < n; ++j) {
-            T ykj = an - bn * j * m_T;
-            T error = m_positions[(last_index - j + N) % N] - ykj;
-            if (std::abs(error) > m_d) {
-                return m_velocity;
-            }
-
-        }
-        m_velocity = bn;
-    }
+T Foaw<T, N>::estimate_velocity() {
+    m_velocity = foaw::estimate_velocity(m_positions, m_position_index, m_T, m_d);
     return m_velocity;
 }
 
