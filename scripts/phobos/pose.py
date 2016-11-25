7d4a10decfbc8212ec3e4643ecbb1e78a288f652
Oliver Lee <oliverzlee@gmail.com>
2016-11-25 14:17:17 +0100

    Return numpy dtype in pose.parse_format
    Return the pose format as a numpy dtype in addition to the Python Struct
    format and as a text description.

--- scripts/phobos/pose.py
+++ scripts/phobos/pose.py
@@ -17,12 +17,13 @@
 import os
 import re
 import struct
-
+import numpy as np
 
 def parse_format(source_file):
     with open(source_file, 'r') as f:
         start_pose_definition = False
-        pose_format = '<'
+        struct_format = '<'
+        np_dtype = []
         pose_format_text = ''
         for line in f:
             if not line:
@@ -38,10 +39,11 @@ def parse_format(source_file):
                 if match: # end of pose definition
                     pose_size = int(match.group(1))
                     pose_format_text += line
-                    struct_size = struct.Struct(pose_format).size
+                    struct_size = struct.Struct(struct_format).size
                     assert struct_size == pose_size, \
                             '{0}, {1}'.format(pose_size, struct_size)
-                    return (pose_format, pose_format_text)
+                    assert np.dtype(np_dtype).itemsize == struct_size
+                    return (struct_format, np.dtype(np_dtype), pose_format_text)
                 else:
                     words = line.split()
                     if words[1][-1] != ';':
@@ -52,9 +54,11 @@ def parse_format(source_file):
 
                     t = words[0]
                     if t == 'float':
-                        pose_format += 'f'
+                        struct_format += 'f'
+                        np_dtype.append((words[1][:-1], 'f4'))
                     elif t == 'uint8_t':
-                        pose_format += 'B'
+                        struct_format += 'B'
+                        np_dtype.append((words[1][:-1], 'u1'))
                     else:
                         msg = 'Conversion for type {0} not handled'.format(t)
                         raise ValueError(msg)
@@ -65,4 +69,7 @@ if __name__ == '__main__':
     local_dir = os.path.dirname(os.path.realpath(__file__))
     project_dir = os.path.join(local_dir, os.path.pardir, os.path.pardir, 'projects')
     flimnap_file = os.path.join(project_dir, 'flimnap', 'main.cc')
-    print(parse_format(flimnap_file))
+    a, b, c = parse_format(flimnap_file)
+    print(a)
+    print(b)
+    print(c)
