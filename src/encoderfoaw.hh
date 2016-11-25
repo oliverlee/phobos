0ea1ed35ebd9e2c019875dc9a0ff620f1a883de5
Oliver Lee <oliverzlee@gmail.com>
2016-11-25 09:57:02 +0100

    Mark EncoderFoaw::velocity as const
    While velocity() requires member variables in EncoderFoaw to be mutable,
    the method is marked const as only the internal fields of the class are
    modified.

--- src/encoderfoaw.hh
+++ src/encoderfoaw.hh
@@ -32,7 +32,7 @@ void EncoderFoaw<T, N>::stop() {
 }
 
 template <typename T, size_t N>
-T EncoderFoaw<T, N>::velocity() {
+T EncoderFoaw<T, N>::velocity() const {
     m_iqhandler.wait();
     T vel = foaw::estimate_velocity(m_iqhandler.circular_buffer(),
             m_iqhandler.index(), m_sample_period, m_allowed_error);
