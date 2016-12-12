c6d05f918d9da9226dd6fefee6ac1e692874cf19
Oliver Lee <oliverzlee@gmail.com>
2016-12-12 15:21:56 +0100

    Add Median filter
    Add Median template class in filter namespace that calculates the median
    of a sequence of values. Values are stored in a circular buffer.

---
+++ inc/filter/median.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <array>
+
+namespace filter {
+
+template <typename T, size_t N>
+class Median {
+    public:
+        Median(T initial_value=static_cast<T>(0));
+        T output() const; /* return median of data in buffer */
+        T output(T input); /* add new value to buffer and return average */
+
+    private:
+        std::array<T, N> m_data; /* circular buffer containing samples */
+        std::array<T, N> m_sorted_data; /* buffer for in-place sorting when finding median */
+        size_t m_data_index; /* index of oldest circular buffer element */
+        T m_median; /* last calculated median */
+};
+
+} // namespace filter
+
+#include "filter/median.hh"
