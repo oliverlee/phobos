28d822308aba066015ff48d7b9f7f95f8daa58a5
Mick van Gelderen <mickvangelderen@gmail.com>
2017-03-09 23:01:35 +0100

    :wrench: Enable COBS length computations at compile time
    cobs::max_encoded_length and cobs::max_decoded_length are now constant expressions.

--- inc/cobs.h
+++ inc/cobs.h
@@ -4,10 +4,37 @@
 #include <cstddef>
 
 namespace cobs {
+    /**
+    Given the length of a decoded byte array, compute the maximum length
+    the encoded byte array can attain.
 
-    size_t max_encoded_length(size_t decoded_length);
+    The maximum length is attained when the decoded byte array contains
+    no zeros. The maximum length consists of the decoded byte array
+    length, the offset overhead and the frame marker. It is computed as
+    decoded_length + floor(decoded_length/254) + 2.
 
-    size_t max_decoded_length(size_t encoded_length);
+    This function does not handle decoded_length < 0.
+    */
+    constexpr size_t max_encoded_length(size_t decoded_length) {
+        return decoded_length + decoded_length/0xfe + 2;
+    }
+
+    /**
+    Given the length of a COBS encoded byte array, compute the maximum
+    length the decoded byte array can attain.
+
+    A case where the minimum overhead is achieved is when the decoded
+    byte array consists only of marker bytes. This means that the
+    maximum decoded length can be as big as the encoded length minus
+    the packet overhead, consisting of the first offset and the marker
+    byte. The maximum decoded length is thus given by encoded_length -
+    2.
+
+    This function does not handle encoded_length < 2.
+    */
+    constexpr size_t max_decoded_length(size_t encoded_length) {
+        return encoded_length - 2;
+    }
 
     struct EncodeResult {
         enum class Status {
