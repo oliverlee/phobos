330ad35613b5ba006f03939262ed9230b4725d9b
Oliver Lee <oliverzlee@gmail.com>
2017-06-22 10:23:07 +0200

    Use more descriptive names in simbicycle

--- projects/src/simbicycle.hh
+++ projects/src/simbicycle.hh
@@ -15,7 +15,7 @@ template <typename Model, typename Observer>
 Bicycle<Model, Observer>::Bicycle(real_t v, real_t dt) :
 m_model(v, dt),
 m_observer(m_model),
-m_state_full(full_state_t::Zero()),
+m_full_state(full_state_t::Zero()),
 m_pose(BicyclePoseMessage_init_zero),
 m_input(input_t::Zero()) {
     // Note: User must initialize Kalman matrices in application.
@@ -79,10 +79,10 @@ void Bicycle<Model, Observer>::update_dynamics(real_t roll_torque_input, real_t 
     // The auxiliary states _must_ also be integrated at the same time as the
     // dynamic state. After the observer update, we "merge" dynamic states together.
     chBSemWait(&m_state_sem);
-    full_state_t x = m_state_full;
+    full_state_t full_state_copy = m_full_state;
     chBSemSignal(&m_state_sem);
-    full_state_t state_full = m_model.integrate_full_state(
-            x, m_input, m_model.dt(), measurement);
+    full_state_t full_state_next = m_model.integrate_full_state(
+            full_state_copy, m_input, m_model.dt(), measurement);
     m_observer.update_state(m_input, measurement);
 
     if (!m_observer.state().allFinite()) {
@@ -122,8 +122,8 @@ void Bicycle<Model, Observer>::update_dynamics(real_t roll_torque_input, real_t 
     //
     // TODO: improve this
     chBSemWait(&m_state_sem);
-    m_state_full = model_t::make_full_state(
-            model_t::get_auxiliary_state_part(state_full),
+    m_full_state = model_t::make_full_state(
+            model_t::get_auxiliary_state_part(full_state_next),
             m_observer.state());
     chBSemSignal(&m_state_sem);
 }
@@ -131,7 +131,7 @@ void Bicycle<Model, Observer>::update_dynamics(real_t roll_torque_input, real_t 
 template <typename Model, typename Observer>
 void Bicycle<Model, Observer>::update_kinematics() {
     chBSemWait(&m_state_sem);
-    full_state_t x = m_state_full;
+    full_state_t x = m_full_state;
     chBSemSignal(&m_state_sem);
 
     // solve for pitch as this does not get integrated
@@ -142,10 +142,10 @@ void Bicycle<Model, Observer>::update_kinematics() {
 
     // update full state with newest computed pitch angle
     chBSemWait(&m_state_sem);
-    model_t::set_full_state_element(m_state_full, full_state_index_t::pitch_angle, pitch);
+    model_t::set_full_state_element(m_full_state, full_state_index_t::pitch_angle, pitch);
 
     // mod rear wheel angle so it does not grow beyond all bounds
-    model_t::set_full_state_element(m_state_full, full_state_index_t::rear_wheel_angle,
+    model_t::set_full_state_element(m_full_state, full_state_index_t::rear_wheel_angle,
             std::fmod(model_t::get_full_state_element(x, full_state_index_t::rear_wheel_angle),
                       constants::two_pi)),
     chBSemSignal(&m_state_sem);
@@ -263,7 +263,7 @@ model::real_t Bicycle<Model, Observer>::dt() const {
 
 template <typename Model, typename Observer>
 const typename Bicycle<Model, Observer>::full_state_t& Bicycle<Model, Observer>::full_state() const {
-    return m_state_full;
+    return m_full_state;
 }
 
 } // namespace sim
