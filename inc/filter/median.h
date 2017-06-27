215bbc525d39ef980423e4be13d4e4f2c767a996
Oliver Lee <oliverzlee@gmail.com>
2017-06-27 23:41:06 +0200

    Refactor comment style in filter classes

--- inc/filter/median.h
+++ inc/filter/median.h
@@ -7,14 +7,14 @@ template <typename T, size_t N>
 class Median {
     public:
         Median(T initial_value=static_cast<T>(0));
-        T output() const; /* return median of data in buffer */
-        T output(T input); /* add new value to buffer and return average */
+        T output() const; // return median of data in buffer
+        T output(T input); // add new value to buffer and return average
 
     private:
-        std::array<T, N> m_data; /* circular buffer containing samples */
-        std::array<T, N> m_sorted_data; /* buffer for in-place sorting when finding median */
-        size_t m_data_index; /* index of oldest circular buffer element */
-        T m_median; /* last calculated median */
+        std::array<T, N> m_data; // circular buffer containing samples
+        std::array<T, N> m_sorted_data; // buffer for in-place sorting when finding median
+        size_t m_data_index; // index of oldest circular buffer element
+        T m_median; // last calculated median
 };
 
 } // namespace filter
--- inc/filter/movingaverage.h
+++ inc/filter/movingaverage.h
@@ -7,13 +7,13 @@ template <typename T, size_t N>
 class MovingAverage {
     public:
         MovingAverage(T initial_value=static_cast<T>(0));
-        T output() const; /* return average of data in buffer */
-        T output(T input); /* add new value to buffer and return average */
+        T output() const; // return average of data in buffer
+        T output(T input); // add new value to buffer and return average
 
     private:
-        std::array<T, N> m_data; /* circular buffer containing samples */
-        size_t m_data_index; /* index of oldest circular buffer element */
-        T m_sum; /* sum of all elements in buffer */
+        std::array<T, N> m_data; // circular buffer containing samples
+        size_t m_data_index; // index of oldest circular buffer element
+        T m_sum; // sum of all elements in buffer
 };
 
 } // namespace filter
