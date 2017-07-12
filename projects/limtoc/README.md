7f036cef4f9f661372e4bf0fbfdcef9b9f98a270
Oliver Lee <oliverzlee@gmail.com>
2017-07-12 10:11:51 +0200

    Add project limtoc
    Add project limtoc which simulates a virtual spring. This project can be
    used to verify that the motor drive have been correctly tuned.

---
+++ projects/limtoc/README.md
@@ -0,0 +1,16 @@
+This project runs static simulator code. It transmits sensor values over serial
+in ASCII and simulates a spring.
+
+The following sensors are used:
+ - steer encoder, TIM5, 115200 counts/rev with index
+ - kistler steer torque voltage, ADC12, 10 kHz
+ - kollmorgen actual torque voltage, ADC13, 10 kHz
+
+The main loop transmits the following at 1 kHz:
+ - short gitsha1
+ - kistler steer torque [N/m]
+ - kollmorgen actual torque [N/m]
+ - steer angle [rad]
+
+A virtual torsional spring with spring constant k = 3.14 N-m/rad is used
+generate the kollmorgen commanded motor torque.
---
+++ projects/limtoc/main.cc
@@ -0,0 +1,110 @@
+/*
+    ChibiOS - Copyright (C) 2006..2015 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#include "ch.h"
+#include "hal.h"
+
+#include "analog.h"
+#include "encoder.h"
+
+#include "gitsha1.h"
+#include "blink.h"
+#include "usbconfig.h"
+#include "saconfig.h"
+#include "utility.h"
+
+#include "parameters.h"
+
+namespace {
+    constexpr uint32_t dt_ms = 1; // ms
+    constexpr float k = 3.14f; // N-m/rad
+
+    // sensors
+    Analog analog;
+    Encoder encoder_steer(sa::RLS_ROLIN_ENC, sa::RLS_ROLIN_ENC_INDEX_CFG);
+
+    dacsample_t set_handlebar_reference(float reference) {
+        constexpr float MAX_REF_VALUE = sa::MAX_KOLLMORGEN_TORQUE;
+        const float saturated_reference = util::clamp(reference, -MAX_REF_VALUE, MAX_REF_VALUE);
+        const dacsample_t aout = saturated_reference/MAX_REF_VALUE*sa::DAC_HALF_RANGE + sa::DAC_HALF_RANGE;
+        dacPutChannelX(sa::KOLLM_DAC, 0, aout); // TODO: don't hardcode zero but find the DAC channel constant
+        return aout;
+    }
+} // namespace
+
+/*
+ * Application entry point.
+ */
+int main(void) {
+
+    //
+    // System initializations.
+    // - HAL initialization, this also initializes the configured device drivers
+    //   and performs the board-specific initializations.
+    // - Kernel initialization, the main() function becomes a thread and the
+    //   RTOS is active.
+    halInit();
+    chSysInit();
+
+    // Initialize a serial-over-USB CDC driver.
+    sduObjectInit(&SDU1);
+    sduStart(&SDU1, &serusbcfg);
+
+    // Activate the USB driver and then the USB bus pull-up on D+.
+    // Note, a delay is inserted in order to not have to disconnect the cable
+    // after a reset.
+    board_usb_lld_disconnect_bus();   //usbDisconnectBus(serusbcfg.usbp);
+    chThdSleepMilliseconds(1500);
+    usbStart(serusbcfg.usbp, &usbcfg);
+    board_usb_lld_connect_bus();      //usbConnectBus(serusbcfg.usbp);
+
+    // create the blink thread 
+    chBlinkThreadCreateStatic();
+
+    // Start sensors.
+    // Encoder:
+    //   Initialize encoder driver 5 on pins PA0, PA1 (EXT2-4, EXT2-8).
+    //   Pins for encoder driver 3 are already set in board.h.
+    palSetLineMode(LINE_TIM5_CH1, PAL_MODE_ALTERNATE(2) | PAL_STM32_PUPDR_FLOATING);
+    palSetLineMode(LINE_TIM5_CH2, PAL_MODE_ALTERNATE(2) | PAL_STM32_PUPDR_FLOATING);
+    encoder_steer.start();
+    analog.start(10000); /* trigger ADC conversion at 10 kHz */
+
+    // Set torque measurement enable line low.
+    // The output of the Kistler torque sensor is not valid until after a falling edge
+    // on the measurement line and it is held low. The 'LINE_TORQUE_MEAS_EN' line is
+    // reversed due to NPN switch Q1.
+    palClearLine(LINE_TORQUE_MEAS_EN);
+    chThdSleepMilliseconds(1);
+    palSetLine(LINE_TORQUE_MEAS_EN);
+
+    // Normal main() thread activity
+    while (true) {
+        const float kistler_torque = util::adc_to_Nm(analog.get_adc12(),
+                sa::KISTLER_ADC_ZERO_OFFSET, sa::MAX_KISTLER_TORQUE);
+        const float motor_torque = util::adc_to_Nm(analog.get_adc13(),
+                sa::KOLLMORGEN_ADC_ZERO_OFFSET, sa::MAX_KOLLMORGEN_TORQUE);
+        const float steer_angle = util::encoder_count<float>(encoder_steer);
+
+        // generate feedback torque to simulate a spring
+        const float feedback_torque = -k*steer_angle;
+        set_handlebar_reference(feedback_torque);
+
+        printf("[%.7s] kistler: %8.3f Nm\tkollmorgen: %8.3f Nm\tsteer: %8.3f rad\r\n",
+               g_GITSHA1, kistler_torque, motor_torque, steer_angle);
+        chThdSleepMilliseconds(dt_ms);
+    }
+}
